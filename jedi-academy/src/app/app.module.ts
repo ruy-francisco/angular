d375c92876effd8f98aae43efe3fab1792cdf92e
ruy-francisco <ruy.fsalmeida@gmail.com>
2018-12-27 11:16:04 -0200

    New components

--- jedi-academy/src/app/app.module.ts
+++ jedi-academy/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StudentComponent } from './student/student.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule
